eac03dd78b6befeb01ab3e9178ac78cc67fc9a89
sybercat <sybercat@163.com>
2026-04-11 01:58:17 +0300

    EX Anecdote

--- Ex3-Anecdotes/src/App.jsx
+++ Ex3-Anecdotes/src/App.jsx
@@ -1,9 +1,71 @@
+import { useState } from 'react'
 
-function App() {
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const AnecdoteInfo = ({title, text, votes}) => {
   return (
-      <div>
+    <>
+      <h1>{title}</h1>
+      {text}<br/>
+      has {votes} votes<br/>
+    </>
+  )
+}
 
-      </div>
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(() => {
+    const initialVotes = {}
+    for (let i = 0; i < anecdotes.length; i++) {
+      initialVotes[i] = 0
+    }
+    return initialVotes
+  })
+  const [maxVoteIndex, setMaxVoteIndex] = useState(0)
+
+
+  const setToSelected = () => {
+    const random = Math.floor(Math.random() * anecdotes.length)
+    setSelected(random)
+  }
+  
+  const setToVotes = () => {
+    const copy = { ...votes }
+    let maxIndex = 0
+    let maxVote = 0
+    // increment the property 2 value by one
+    copy[selected] += 1 
+    setVotes(copy)
+    for (let i = 0; i < anecdotes.length; i++) {
+      if(maxVote < copy[i]){
+        maxIndex = i
+        maxVote = copy[i]
+      }
+    }
+    setMaxVoteIndex(maxIndex)
+  }
+
+  return (
+    <div>
+      <AnecdoteInfo title = "Anecdote of the day" text = {anecdotes[selected]} votes = {votes[selected]}/>
+      <Button handleClick={setToVotes} text="vote" />
+      <Button handleClick={setToSelected} text="next Anecdote" />
+      <AnecdoteInfo title = "Anecdote with most votes" text = {anecdotes[maxVoteIndex]} votes = {votes[maxVoteIndex]}/>
+    </div>
   )
 }
 
